94adc063084a7b0238254f37ed2aefd3154b7d42
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-24 14:27:36 -0700

    WIP. storage. clean up old atoms

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,24 +1,16 @@
 import { Atom, atom, Getter, Setter } from 'jotai';
 import { Atomize, atomWithCallback } from '@/hooks/atomsX';
 import { debounce } from '@/utils/debounce';
-import { initalValueShapeParams } from './ngon/shape-defaults';
-import { NewShapeParams, ShapeGadgets } from './ngon/shape';
-import { defaultShapes } from './ngon/shapes-vault-data';
-import { generate } from './ngon/generator';
-import { IO, } from './ngon/shape-io';
 import { toastError } from '@/components/UI/UiToaster';
 import { uuid } from '@/utils/uuid';
 
 //#region LocalStorage
 
 namespace Storage {
-    const KEY = 'react-svg-shapes22-01';
+    const KEY = 'react-installation-page';
 
     type Store = {
         open: StorageOpenSections;
-        shapeParams: NewShapeParams;
-        viewboxOptions: StorageViewboxOptions;
-        vaultData: StorageVaultData;
     };
 
     export let initialData: Store = {
@@ -27,20 +19,6 @@ namespace Storage {
             presets: false,
             variants: false,
         },
-        shapeParams: initalValueShapeParams(),
-        viewboxOptions: {
-            showBox: false,
-            showUtils: false,
-
-            showAll: false,
-            showInnerLines: true,
-            showOuterLines: true,
-            showInnerDots: true,
-            showOuterDots: true,
-        },
-        vaultData: {
-            shapes: [],
-        }
     };
 
     function load() {
@@ -52,8 +30,6 @@ namespace Storage {
             } catch (error) {
             }
         }
-
-        initialData.vaultData.shapes = initialData.vaultData?.shapes?.length ? initialData.vaultData.shapes : defaultShapes;
     }
     load();
 
@@ -63,20 +39,6 @@ namespace Storage {
                 source: get(openSections.sourceAtom),
                 presets: get(openSections.presetsAtom),
                 variants: get(openSections.variantsAtom),
-            },
-            shapeParams: get(editorShapeParamsAtom),
-            viewboxOptions: {
-                showBox: get(viewboxOptionAtoms.showBoxAtom),
-                showUtils: get(viewboxOptionAtoms.showUtilsAtom),
-
-                showAll: get(viewboxOptionAtoms.showAllAtom),
-                showInnerLines: get(viewboxOptionAtoms.showInnerLinesAtom),
-                showOuterLines: get(viewboxOptionAtoms.showOuterLinesAtom),
-                showInnerDots: get(viewboxOptionAtoms.showInnerDotsAtom),
-                showOuterDots: get(viewboxOptionAtoms.showOuterDotsAtom),
-            },
-            vaultData: {
-                shapes: get(_shapesAtom),
             },
         };
         localStorage.setItem(KEY, JSON.stringify(newStore));
@@ -102,102 +64,3 @@ export const openSections: Atomize<StorageOpenSections> = {
 };
 
 //#endregion UI sections state
-
-//#region Editor controls
-
-type StorageViewboxOptions = {
-    showBox: boolean;           // show box controls
-    showUtils: boolean;         // show utility controls
-} & ShapeGadgets;
-
-export const viewboxOptionAtoms: Atomize<StorageViewboxOptions> = {
-    showBoxAtom: atomWithCallback<boolean>(Storage.initialData.viewboxOptions.showBox, Storage.save),
-    showUtilsAtom: atomWithCallback<boolean>(Storage.initialData.viewboxOptions.showUtils, Storage.save),
-
-    showAllAtom: atomWithCallback<boolean>(Storage.initialData.viewboxOptions.showAll, Storage.save),
-    showInnerLinesAtom: atomWithCallback<boolean>(Storage.initialData.viewboxOptions.showInnerLines, Storage.save),
-    showOuterLinesAtom: atomWithCallback<boolean>(Storage.initialData.viewboxOptions.showOuterLines, Storage.save),
-    showInnerDotsAtom: atomWithCallback<boolean>(Storage.initialData.viewboxOptions.showInnerDots, Storage.save),
-    showOuterDotsAtom: atomWithCallback<boolean>(Storage.initialData.viewboxOptions.showOuterDots, Storage.save),
-};
-
-// Editor shape
-
-export const editorShapeParamsAtom = atomWithCallback(Storage.initialData.shapeParams, Storage.save);
-
-export const editorShapeAtom = atom(
-    (get) => {
-        const params = get(editorShapeParamsAtom);
-        const shape = generate(params);
-        return shape;
-    }
-);
-
-//#endregion Editor controls
-
-//#region Vault loader
-
-type StorageVaultData = {
-    shapes: string[];
-};
-
-const _shapesAtom = atom<string[]>(Storage.initialData.vaultData.shapes); // keep it as atom to reduce valid shapes packing when shape params changed.
-
-type VaultSpapesArray = {
-    valid: IO.ConvertResult[];
-    failed: string[];
-};
-
-export const vaultSpapes: Atomize<VaultSpapesArray> = {
-    validAtom: atomWithCallback<IO.ConvertResult[]>([], saveVaultShapes),
-    failedAtom: atomWithCallback<string[]>([], saveVaultShapes),
-};
-
-function saveVaultShapes({ get, set }: { get: Getter, set: Setter; }) {
-    const valid = get(vaultSpapes.validAtom);
-    const failed = get(vaultSpapes.failedAtom);
-    set(_shapesAtom, [...IO.convertResultForVault(valid), ...failed]);
-    Storage.saveDebounced(get);
-}
-
-const runFetchVaultShapesAtom = atom(
-    () => null,
-    (_get, set) => {
-        const { parsedShapes, failedShapes, } = IO.parseVaultShapes(Storage.initialData.vaultData.shapes);
-        set(vaultSpapes.validAtom, parsedShapes);
-        set(vaultSpapes.failedAtom, failedShapes); //toastError('test');
-    }
-);
-runFetchVaultShapesAtom.onMount = (runFetch) => runFetch();
-
-export const dataLoadAtom = atom(
-    (get) => {
-        get(runFetchVaultShapesAtom);
-    }
-);
-
-//#endregion Vault loader
-
-//#region Vault operations
-
-export const doSaveToVaultAtom = atom(null,
-    (get, set,) => {
-        const shapeParams = { ...get(editorShapeParamsAtom) };
-        const shape = generate(shapeParams);
-        const gadgets = undefined;
-
-        shapeParams.id = get(vaultSpapes.validAtom).find(({ id }) => id === shapeParams.id) ? uuid() : shapeParams.id;
-
-        set(vaultSpapes.validAtom, (p) => [...p, { id: shapeParams.id, shapeParams, shape, gadgets }]);
-    }
-);
-
-export const doRemoveFromVaultAtom = atom(null, //TODO: should have access by Id or by index is enough?
-    (get, set, idx: number) => {
-        const shapes = get(vaultSpapes.validAtom);
-        const newShapes = shapes.slice(0, idx).concat(shapes.slice(idx + 1));
-        set(vaultSpapes.validAtom, newShapes);
-    }
-);
-
-//#endregion Vault operations
