f3268dfa001a80c1bdc0b0c0fa11a0a88b859ade
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-24 14:35:49 -0700

    WIP. App3_Footer. update layout

--- src/components/App3_Footer.tsx
+++ src/components/App3_Footer.tsx
@@ -1,25 +1,26 @@
-import React, { HTMLAttributes } from 'react';
+import React, { AnchorHTMLAttributes, HTMLAttributes } from 'react';
 import { classNames } from '@/utils/classnames';
 import { IconGithubLogo } from './UI/UIIcons';
 
-function GitHubLink({href, className, ...rest}: {href: string} & HTMLAttributes<SVGSVGElement>) {
+function Link({ className, children, ...rest }: AnchorHTMLAttributes<HTMLAnchorElement>) {
     return (
-        <a href={href} target="_blank">
-            <IconGithubLogo
-                className={classNames("w-5 h-5 fill-primary-200 stroke-[50] stroke-current hover:stroke-primary-200 hover:scale-[1.5] hover:stroke-[34] transition-all", className)}
-                title="Open the source code of the project on Github"
-                {...rest}
-            />
+        <a target="_blank" className={classNames(" hover:text-primary-200 hover:scale-y-110 transition-all", className)} {...rest}>
+            {children}
         </a>
     );
 }
 
 export function App3_Footer() {
     return (
-        <div className="px-4 py-2 text-sm flex items-center justify-end space-x-1 bg-title3 text-primary-400/75">
-            {/* <GitHubLink href="https://github.com/maxzz/ngon-gen" title="Open the old project source code on Github" />
-            <GitHubLink href="https://maxzz.github.io/ngon-gen" title="Open the old project app" />
-            <GitHubLink href="https://github.com/maxzz/ngon-gen22" /> */}
+        <div className="px-2 sm:px-4 py-2 text-sm flex items-center justify-between space-x-1 bg-title3 text-primary-400/75">
+            <Link href="https://www.hidglobal.com/products/software/digitalpersona/digitalpersona" className="text-[.65rem] leading-3 md:text-xs transition-all">
+                <span className="inline-block">© 2022 HID Global Corporation, part of ASSA ABLOY.</span> <span className="inline-block">All rights reserved.</span>
+            </Link>
+            <div className="flex items-center space-x-4">
+                <Link href="https://www.hidglobal.com/sites/default/files/crossmatch/AltusAddons/g01/PrivacyPolicy.txt" target="_blank">Privacy policy</Link>
+                <Link href="https://www.hidglobal.com/sites/default/files/crossmatch/AltusAddons/g01/pagedp/EULA.html" target="_blank">End User License Agreement</Link>
+                {/* title="Open the source code of the project on Github" */}
+            </div>
         </div>
     );
 }
