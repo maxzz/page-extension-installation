8f1c403d26c5edeb135d6e8d11114e6222e15cb6
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-07 17:52:23 -0800

    WIP. App1_Header. add link to the old installation page

--- src/components/App1_Header.tsx
+++ src/components/App1_Header.tsx
@@ -8,9 +8,11 @@ export function App1_Header() {
     return (<>
         <div className="px-6 py-5 flex items-center justify-between bg-title3 shadow-sm cursor-default"> {/* bg-title4 */}
             <div className="flex items-center space-x-4">
-                <div className="w-20 py-2 flex items-center justify-center bg-white rounded-md">
-                    <IconHIDLogo className="px-2 fill-title5" stroke="currentColor" strokeWidth={0.2} fill="none" />
-                </div>
+                <a href="https://www.hidglobal.com/sites/default/files/crossmatch/AltusAddons/g01/pagedp/index.html" className="cursor-pointer">
+                    <div className="w-20 py-2 flex items-center justify-center bg-white rounded-md">
+                        <IconHIDLogo className="px-2 fill-title5" stroke="currentColor" strokeWidth={0.2} fill="none" />
+                    </div>
+                </a>
                 <h1 className="pb-1 text-sm md:text-xl tracking-tighter font-light text-slate-100 whitespace-normal sm:whitespace-nowrap" style={textShadow}>
                     <span className="inline-block">DigitalPersona</span>
                     <span className="inline-block">&#174;</span> Password Manager Extension <span className="inline-block">for Chrome / Microsoft Edge</span>
