558a7e7d989ad97c814a68c616716c584f7ef11d
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-07 17:49:18 -0800

    WIP. App3_Footer. add link to the new www3 domain

--- src/components/App3_Footer.tsx
+++ src/components/App3_Footer.tsx
@@ -10,10 +10,13 @@ function Link({ className, children, ...rest }: AnchorHTMLAttributes<HTMLAnchorE
     );
 }
 
+const wwwLink = 'https://www.hidglobal.com/products/software/digitalpersona/digitalpersona';
+const www3Link = 'https://www3.hidglobal.com/sites/default/files/resource_files/iams-digitalpersona-premium-br-en_1.pdf';
+
 export function App3_Footer() {
     return (
         <div className="px-2 sm:px-4 py-2 text-sm flex items-center justify-between space-x-1 bg-title3 text-primary-400/75 text-[.65rem] leading-3 ">
-            <Link href="https://www.hidglobal.com/products/software/digitalpersona/digitalpersona" className="md:text-xs transition-all">
+            <Link href={wwwLink} className="md:text-xs transition-all">
                 <span className="inline-block">© 2022 HID Global Corporation, part of ASSA ABLOY.</span> <span className="inline-block">All rights reserved.</span>
             </Link>
             <div className="flex items-center space-x-4">
