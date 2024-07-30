5993e2c64bc6866c710b820fd5298d8f42457bd9
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-29 19:10:01 -0700

    WIP. update package dependencies. vite to v5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
 import path from 'node:path';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import visualizer from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer';
 import replace from '@rollup/plugin-replace';
 import url from '@rollup/plugin-url';
 
@@ -43,5 +43,8 @@ export default (({ command }) => defineConfig({
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
-    }
+    },
+    server: {
+        port: 3000,
+    },
 }));
